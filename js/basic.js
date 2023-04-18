35f748a5acc049ce210ef453c9e0440d69262577
JTosman <josmanfrancisco1311@gmail.com>
2023-04-18 09:06:31 -0430

    rawi

---
+++ js/basic.js
@@ -0,0 +1,14 @@
+function Search (){
+
+	let search_button = document.querySelector('.search-button');
+
+	search_button.style.borderBottom = '2px solid white';
+	search_button.style.transition = '0.1s';
+}
+
+function Blur (){
+
+	let search_button = document.querySelector('.search-button');
+
+	search_button.style.borderBottom = '1px solid white';
+}
